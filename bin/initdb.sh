3d6e74331c5551c7d7aef73669fcd11394e62c17
yangxiaodong <yangxiaodong@qianmi.com>
2020-05-12 14:00:55 +0800

    add: docker 部署起来..

--- bin/initdb.sh
+++ bin/initdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #mysqladmin -u root -proot create davinci0.3
-mysql -P 3306 -h localhost -u root -proot davinci0.3 < $DAVINCI3_HOME/bin/davinci.sql
+#mysql -P 3306 -h localhost -u root -proot davinci0.3 < $DAVINCI3_HOME/bin/davinci.sql
